8a528e5bdd3035a086cf97f7d7bcae995d2abe68
pierre <pierre@techandrun.com>
2017-01-01 18:41:49 +0100

    Version Alpha 2

--- src/course_generator/frmMain.java
+++ src/course_generator/frmMain.java
@@ -218,7 +218,7 @@ import course_generator.utils.Utils.CalcLineResult;
  * @author pierre.delore
  */
 public class frmMain extends javax.swing.JFrame {
-	private final String Version = "4.0.0.ALPHA 1";
+	private final String Version = "4.0.0.ALPHA 2";
 //	private FileFilter GPX_Filter;
 	public TrackData Track;
 	private ResumeData Resume;
