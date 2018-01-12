916776c036d83a62fc277a10df0a7d88e4999a55
FJBDev <frederic@freemovin.com>
2018-01-12 15:19:01 -0700

    Fixing the pdf doc generation and adding the latest generated pdf file

--- doc/fr/build_pdf.sh
+++ doc/fr/build_pdf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pandoc --toc --css cg_doc.css --pdf-engine=xelatex -o cg_doc_4.00.pdf md/00-Title.md md/01-Introduction.md md/02-Interface.Principle md/md-03.md md/04-Menus.md md/05-Toolbars.md md/06-Statusbar.md md/07-Tabs.md md/08-Use.md md/09-Advanced_use.md md/10-Tools.md
+pandoc --toc --css cg_doc.css --pdf-engine=xelatex -o cg_doc_4.00.pdf md/00-Title.md md/01-Introduction.md md/02-Principle.md md/03-Interface.md md/04-Menus.md md/05-Toolbars.md md/06-Statusbar.md md/07-Tabs.md md/08-Use.md md/09-Advanced_use.md md/10-Tools.md
 
