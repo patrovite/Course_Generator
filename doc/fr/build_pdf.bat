d95156f78b32315f4c094c8eaeaa31bc2913f4c3
FJBDev <frederic@freemovin.com>
2018-01-12 14:32:56 -0700

    Specifying the latex engine

--- doc/fr/build_pdf.bat
+++ doc/fr/build_pdf.bat
@@ -1,1 +1,1 @@
-d:\PersoPDE\Pandoc\pandoc.exe --toc --css pandoc.css -o cg.pdf 01-Introduction\01-Introduction.md 02-Principle\02-Principle.md 03-Interface\03-Interface.md
+d:\PersoPDE\Pandoc\pandoc.exe --toc --css pandoc.css --pdf-engine=xelatex -o cg.pdf 01-Introduction\01-Introduction.md 02-Principle\02-Principle.md 03-Interface\03-Interface.md
