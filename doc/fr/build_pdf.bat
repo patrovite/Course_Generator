c1e623de85fd07aefda3eb4873218ded53fa23a7
FJBDev <frederic@freemovin.com>
2018-01-12 15:22:52 -0700

    Updating the pdf bat file

--- doc/fr/build_pdf.bat
+++ doc/fr/build_pdf.bat
@@ -1,1 +1,1 @@
-pandoc --toc --css pandoc.css --pdf-engine=xelatex -o cg.pdf 01-Introduction\01-Introduction.md 02-Principle\02-Principle.md 03-Interface\03-Interface.md
+pandoc --toc --css cg_doc.css --pdf-engine=xelatex -o cg_doc_4.00.pdf md/00-Title.md md/01-Introduction.md md/02-Principle.md md/03-Interface.md md/04-Menus.md md/05-Toolbars.md md/06-Statusbar.md md/07-Tabs.md md/08-Use.md md/09-Advanced_use.md md/10-Tools.md
