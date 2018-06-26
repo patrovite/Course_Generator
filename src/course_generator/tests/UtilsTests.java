6bd370ffde812138b08d0a2fa9d990e544533911
FreeMovin <frederic@freemovin.com>
2018-06-26 16:58:52 +0000

    Adding unit tests for SpeedTopaceNumber

--- src/course_generator/tests/UtilsTests.java
+++ src/course_generator/tests/UtilsTests.java
@@ -21,6 +21,15 @@ public class UtilsTests {
 		double expectedSpeed = 5.7; // mph
 		double actualSpeed = Utils.PaceToSpeed("10:30");
 		assertEquals(expectedSpeed, actualSpeed, 0.1);
+
+		// {@see course_generator.utils.Utils#SpeedToPaceNumber(double)}
+		expectedSpeed = 10.0; // 10 min/mile
+		actualSpeed = Utils.SpeedToPaceNumber(6.0); // 6 mph
+		assertEquals(expectedSpeed, actualSpeed, 0.1);
+
+		expectedSpeed = 7.42; // 7min42sec/mile
+		actualSpeed = Utils.SpeedToPaceNumber(7.7); // 7.7mph
+		assertEquals(expectedSpeed, actualSpeed, 0.2);
 	}
 
 }
