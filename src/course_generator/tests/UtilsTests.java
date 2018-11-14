ebdf9e961da928bf11fae4c1e33f193b50978481
FJBDev <25235665+FJBDev@users.noreply.github.com>
2018-11-14 20:07:14 +0000

    Exhaustive unit tests (100% successful)

--- src/course_generator/tests/UtilsTests.java
+++ src/course_generator/tests/UtilsTests.java
@@ -35,6 +35,7 @@ public class UtilsTests {
 
 		// {@see course_generator.utils.Utils#SpeedCurrentUnitsToMeters(double,
 		// CgSettings)}
+		// 10 min/mile ==> 9.656064 km/h
 		expectedSpeed = 9.656064; // 9.656064 km/h
 		CgSettings settings = new CgSettings();
 		settings.Unit = CgConst.UNIT_MILES_FEET;
@@ -42,18 +43,28 @@ public class UtilsTests {
 		actualSpeed = Utils.SpeedCurrentUnitsToMeters(10.0, settings); // 10 min/mile
 		assertEquals(expectedSpeed, actualSpeed, 0.2);
 
+		// 6mph ==> 9.656064 km/h
+		expectedSpeed = 9.656064; // 9.656064 km/h
+		settings.Unit = CgConst.UNIT_MILES_FEET;
+		settings.isPace = false;
+		actualSpeed = Utils.SpeedCurrentUnitsToMeters(6.0, settings); // 6mph
+		assertEquals(expectedSpeed, actualSpeed, 0.2);
+
+		// 16 km/h ==> 6.2 min/mile
 		expectedSpeed = 6.2; // 6.2 min/mile
 		settings.Unit = CgConst.UNIT_MILES_FEET;
 		settings.isPace = true;
 		actualSpeed = Utils.SpeedMeterToCurrentUnits(16, settings); // 16 km/h
 		assertEquals(expectedSpeed, actualSpeed, 0.2);
 
+		// 12 km/h ==> 12 km/h
 		expectedSpeed = 12.0; // 12 km/h
 		settings.Unit = CgConst.UNIT_METER;
 		settings.isPace = false;
 		actualSpeed = Utils.SpeedMeterToCurrentUnits(12, settings); // 12 km/h
 		assertEquals(expectedSpeed, actualSpeed, 0.2);
 
+		// 13 km/h ==> 4mn36 /km
 		expectedSpeed = 4.36; // 4 min 36 sec / km
 		settings.Unit = CgConst.UNIT_METER;
 		settings.isPace = true;
