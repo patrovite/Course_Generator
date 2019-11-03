625bbba04adf628ff98a2a7260981056db0e20a5
Pierre Delore <pierre@TechAndRun.com>
2019-11-03 10:45:21 +0100

    Update zip generation file to add the 'curevs' folder

--- src/course_generator/param/frmEditCurve.java
+++ src/course_generator/param/frmEditCurve.java
@@ -62,6 +62,7 @@ import org.jfree.data.xy.XYSeriesCollection;
 import course_generator.TrackData;
 import course_generator.settings.CgSettings;
 import course_generator.utils.CgConst;
+import course_generator.utils.CgLog;
 import course_generator.utils.Utils;
 
 public class frmEditCurve extends javax.swing.JDialog {
--- tools/build_zip.sh
+++ tools/build_zip.sh
@@ -7,6 +7,7 @@ cd ../build
 # Add all the files and dir of the "build" dir
 zip -r ../distrib/Course_Generator_x.x.x.Linux.MacOS.Windows.portable.zip help *.*
 zip -r ../distrib/Course_Generator_x.x.x.Linux.MacOS.Windows.portable.zip course_generator_lib *.*
+zip -r ../distrib/Course_Generator_x.x.x.Linux.MacOS.Windows.portable.zip curves *.*
 
 # Add the licence file
 cd ..
