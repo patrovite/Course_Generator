a5be30a0e07144e29e521b7cdcfb369b50634f63
FJBDev <25235665+FJBDev@users.noreply.github.com>
2020-10-18 20:59:49 -0600

    Revert

--- src/course_generator/utils/Utils.java
+++ src/course_generator/utils/Utils.java
@@ -158,7 +158,7 @@ public class Utils {
 	 * @param inEclipse true if we are inside eclipse
 	 */
 	public static void SetProgDir(boolean inEclipse) {
-		ProgDir = new File("/home/frederic/Documents/Projects/CourseGenerator").getAbsolutePath();
+		ProgDir = new File(".").getAbsolutePath();
 		ProgDir = ProgDir.replaceAll("\\\\", "/");
 		if (ProgDir.endsWith("/."))
 			ProgDir = ProgDir.substring(0, ProgDir.length() - 2);
